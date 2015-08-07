d3e43b0a0bd11a024d7664348088991365e9e076
unknown <pstalcup@Viper.phoenix-int.com>
2015-08-07 17:28:44 -0400

    Missed these files in the last commit

--- app.js
+++ app.js
@@ -1,55 +1,84 @@
 (function(){
-	// metadata about the game
+	//TODO: Register a service to hold game data
+
 	var gameInfo = {
-		name: "Rocket Fuel",
-		author: "Patrick Stalcup",
-		version: "1.0.0"
+		name: 'Rocket Fuel',
+		author: 'Patrick Stalcup',
+		version: '1.0.0'
 	}
 
 	var heros = [{
-			name: "Patrick",
+			name: 'Patrick',
 			hp: {
 				current: 80,
 				max: 100
-			}
+			},
+			actions: [
+				{
+					text:"Fight",
+					options: ["A","B","C"]
+				}, {
+					text:"Run",
+					options: ["A","B","C"]
+				}],
+			selected: true
 		},{
-			name: "Will",
+			name: 'Will',
 			hp: {
 				current: 20,
 				max: 20
-			}
+			},
+			actions: [
+				{
+					text:"Fight",
+					options: ["A","B","C"]
+				}, {
+					text:"Run",
+					options: ["A","B","C"]
+				}],
+			selected: false
 		}
-	]
+	];
 	var villains = [{
-			name: "Snake",
+			name: 'Snake',
 			hp: {
 				current: 10,
 				max: 10
 			}
 		},{
-			name: "Rat",
+			name: 'Rat',
 			hp: {
 				current: 20,
 				max: 20
 			}
 		}
-	]
+	];
+	var messages = [];
 
-
-	var app = angular.module("rpg", []);
+	var app = angular.module('rpg', []);
 
 	//controllers
-	app.controller("CombatController", ['$scope', function($scope){
+	app.controller('CombatController', ['$scope', '$timeout', function($scope, $timeout){
 		$scope.heros = heros; 
 		$scope.villains = villains; 
+
+		$scope.alert = function(message, type) {
+			if (type === undefined) {
+				type = 'info'; 
+			}
+			messages.push({type: type, content: message}); 
+			$timeout(function(){
+				messages.shift(); 
+			}, 4000);
+		}
 	}]); 
 
 	//directives
-	app.directive("gameInfo", function(){
+	app.directive('gameInfo', function(){
 		return {
-			restrict: "E",
-			controllerAs: "info",
-			templateUrl: "templates/game-info.html",
+			restrict: 'E',
+			controllerAs: 'info',
+			templateUrl: 'templates/game-info.html',
 			controller: function(){
 				this.gameInfo = gameInfo; 
 			}
@@ -58,17 +87,41 @@
 
 	app.directive('characterPortraits', function(){
 		return {
-			restrict: "E",
+			restrict: 'E',
 			scope: {
 				characters: '=characters'
 			},
-			templateUrl: "templates/character-portraits.html",
-			controllerAs: "portrait",
+			templateUrl: 'templates/character-portraits.html',
+			controllerAs: 'portrait',
 			controller: function(){
 				this.percentage = function(hp) {
 					return parseInt(100 * (hp.current / hp.max)); 
 				}
 			}
+		};
+	});
+
+	app.directive('combatMessages', function(){
+		return {
+			templateUrl: 'templates/combat-messages.html',
+			controller: ['$scope', function($scope) {
+				$scope.messages = messages; 
+			}]
+		};
+	});
+
+	app.directive('combatActions', function(){
+		return {
+			templateUrl: 'templates/combat-actions.html',
+			controller: ['$scope', function($scope) {
+				$scope.characters = heros; 
+				$scope.previousAction = {}; 
+				$scope.act = function(action) {
+					$scope.previousAction.selected = false; 
+					action.selected = true; 
+					$scope.previousAction = action; 
+				}
+			}]
 		};
 	});
 })(); 

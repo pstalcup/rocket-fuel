50025fd86888f37e1b7681dd4b4d730ad7b44184
unknown <pstalcup@Viper.phoenix-int.com>
2015-08-07 15:35:10 -0400

    Added more directives, cleaned up the index html. Still includes some styles/elements meant to be expanded on later

--- app.js
+++ app.js
@@ -6,9 +6,45 @@
 		version: "1.0.0"
 	}
 
+	var heros = [{
+			name: "Patrick",
+			hp: {
+				current: 80,
+				max: 100
+			}
+		},{
+			name: "Will",
+			hp: {
+				current: 20,
+				max: 20
+			}
+		}
+	]
+	var villains = [{
+			name: "Snake",
+			hp: {
+				current: 10,
+				max: 10
+			}
+		},{
+			name: "Rat",
+			hp: {
+				current: 20,
+				max: 20
+			}
+		}
+	]
+
+
 	var app = angular.module("rpg", []);
 
-	//directive for generating a content div
+	//controllers
+	app.controller("CombatController", ['$scope', function($scope){
+		$scope.heros = heros; 
+		$scope.villains = villains; 
+	}]); 
+
+	//directives
 	app.directive("gameInfo", function(){
 		return {
 			restrict: "E",
@@ -17,6 +53,22 @@
 			controller: function(){
 				this.gameInfo = gameInfo; 
 			}
-		}
+		};
+	});
+
+	app.directive('characterPortraits', function(){
+		return {
+			restrict: "E",
+			scope: {
+				characters: '=characters'
+			},
+			templateUrl: "templates/character-portraits.html",
+			controllerAs: "portrait",
+			controller: function(){
+				this.percentage = function(hp) {
+					return parseInt(100 * (hp.current / hp.max)); 
+				}
+			}
+		};
 	});
 })(); 
